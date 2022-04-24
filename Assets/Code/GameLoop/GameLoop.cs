658dfd0d77d62d22394c3bcf2a5524b306fa9cac
Andrei Raketa <75320766+raketa420gh@users.noreply.github.com>
2022-04-24 21:10:00 +0300

    Game loop refactoring

--- Assets/Code/GameLoop/GameLoop.cs
+++ Assets/Code/GameLoop/GameLoop.cs
@@ -7,12 +7,9 @@ public class GameLoop : MonoBehaviour
     public LoadLevelState LoadLevelState;
     public StartLevelState StartLevelState;
     public EndLevelState EndLevelState;
-
-    private LevelLoop _currentLevelLoop;
+    
     private EnemySpawner _enemySpawner;
     private Turret _turret;
-
-    public EnemySpawner EnemySpawner => _enemySpawner;
 
     [Inject]
     public void Construct(EnemySpawner enemySpawner, Turret turret)
@@ -26,27 +23,12 @@ public class GameLoop : MonoBehaviour
         InitializeStateMachine();
     }
 
-    public void SetCurrentLevelLoop(LevelLoop levelLoop)
-    {
-        _currentLevelLoop = levelLoop;
-    }
-
-    public void StartTurretShooting()
-    {
-        InvokeRepeating(nameof(TurretShootToTarget), 0, 0.2f);
-    }
-
-    private void TurretShootToTarget()
-    {
-        _turret.ShootToTarget();
-    }
-
     private void InitializeStateMachine()
     {
         GameLoopStateMachine = new StateMachine();
 
         LoadLevelState = new LoadLevelState(this, GameLoopStateMachine);
-        StartLevelState = new StartLevelState(this, GameLoopStateMachine);
+        StartLevelState = new StartLevelState(this, GameLoopStateMachine, _enemySpawner, _turret);
         EndLevelState = new EndLevelState(this, GameLoopStateMachine);
 
         GameLoopStateMachine.ChangeState(LoadLevelState);
--- Assets/Code/GameLoop/StartLevelState.cs
+++ Assets/Code/GameLoop/StartLevelState.cs
@@ -1,20 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+
 public class StartLevelState : GameLoopState
 {
     private LevelLoop _levelLoop;
     private EnemySpawner _enemySpawner;
+    private Turret _turret;
     
-    public StartLevelState(GameLoop gameLoop, StateMachine stateMachine) : base(gameLoop, stateMachine)
+    public StartLevelState(GameLoop gameLoop, StateMachine stateMachine, EnemySpawner enemySpawner, Turret turret) : base(gameLoop, stateMachine)
     {
+        _enemySpawner = enemySpawner;
+        _turret = turret;
     }
 
     public override void Enter()
     {
         base.Enter();
-
-        _levelLoop = new LevelLoop(gameLoop.EnemySpawner);
-        gameLoop.SetCurrentLevelLoop(_levelLoop);
+        _levelLoop = new LevelLoop(_enemySpawner);
         _levelLoop.StartLevel();
-        
-        gameLoop.StartTurretShooting();
+        StartTurretShooting(0.2f);
+    }
+
+    private void TurretShootToTarget()
+    {
+        _turret.ShootToTarget();
+    }
+    
+    private async Task StartTurretShooting(float period, float startDelay = 0f)
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(startDelay));
+
+        while (true)
+        {
+            TurretShootToTarget();
+            await UniTask.Delay(TimeSpan.FromSeconds(period));
+        }
     }
 }
